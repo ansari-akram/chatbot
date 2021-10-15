4a1b8c4a09cd8d95217648e6ba7fc16dfccb3adb
ansari-akram <ansariakramobaid@gmail.com>
2021-10-16 01:35:28 +0530

    my first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ChatEngComponent } from './chat-eng/chat-eng.component';
+import { ChatArbComponent } from './chat-arb/chat-arb.component'
+
+const routes: Routes = [
+  {path:'english',component:ChatEngComponent},
+  {path:'arabic',component:ChatArbComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
